bad6b39fd9c4c12caf7676fda32f63c0c30ff9ec
s1280033 <s1280033@u-aizu.ac.jp>
2021-07-08 10:29:52 +0900

    add new file

---
+++ DiceGame.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+  int d1,d2;
+  d1 = rand() % 6 + 1;
+  d2 = rand() % 6 + 1;
+  printf("Rolling the dice...\n");
+  printf("Die 1: %d\n",d1);
+  printf("Die 2: %d\n",d2); 
+  printf("Total value: %d\n", d1 + d2);
+}
